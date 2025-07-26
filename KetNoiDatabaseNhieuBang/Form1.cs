fff625e914c4b643b98aa4efc8dc7b0b9c628c0a
phucnv46 <phucnv46@fpt.edu.vn>
2025-07-26 11:05:48 +0700

    Upload

---
+++ KetNoiDatabaseNhieuBang/Form1.cs
@@ -0,0 +1,14 @@
+namespace KetNoiDatabaseNhieuBang
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+
+      
+    }
+}
---
+++ KetNoiDatabaseNhieuBang/Models/TacGium.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KetNoiDatabaseNhieuBang.Models;
+
+public partial class TacGium
+{
+    public int MaTacGia { get; set; }
+
+    public string HoTen { get; set; } = null!;
+
+    public string? QuocTich { get; set; }
+
+    public virtual ICollection<Sach> Saches { get; set; } = new List<Sach>();
+}
