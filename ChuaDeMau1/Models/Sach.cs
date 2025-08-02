d1beb2a0ebc0441f72b065cf4516e8bf4df047e4
phucnv46 <phucnv46@fpt.edu.vn>
2025-08-02 10:56:39 +0700

    upload

---
+++ ChuaDeMau1/Models/Sach.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChuaDeMau1.Models;
+
+public partial class Sach
+{
+    public string MaSach { get; set; } = null!;
+
+    public string? TenSach { get; set; }
+
+    public decimal? DonGia { get; set; }
+
+    public int? SoTrang { get; set; }
+}
